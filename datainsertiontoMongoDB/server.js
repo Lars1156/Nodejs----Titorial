5d1fdde93a9ce72b689bce03e0c5184adbff8f93
Optimus115 <91112019+Optimus115@users.noreply.github.com>
2024-05-28 20:59:05 +0530

    enter the data on the mongoDB file

---
+++ datainsertiontoMongoDB/server.js
@@ -0,0 +1,23 @@
+const {MongoClient} = require('mongodb');
+const uri ="mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+const databasename = "sample_data";
+const collectionName = "data_insertion";
+
+async function insertData(data){
+    try{
+        await client.connect();
+        const database =client.db(databasename);
+        const collection = database.collection(collectionName);
+
+        const result = await collection.insertOne(data)
+        console.log(`Doccument inserted with Id : ${result.insertedId}`);
+    }catch(error){
+        console.error(error);
+    }finally{
+        await client.close();
+    }
+}
+const myData ={name:"Jone Doe",age:30 , address:"Hinjawadi Phase one "};
+insertData(myData);
